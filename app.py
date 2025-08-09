35d729e92f16411441edb082912e070948fa6c15
akshay-node <abhishek.pathania@ditstek.com>
2025-08-09 16:08:37 +0530

    all done

--- app.py
+++ app.py
@@ -18,6 +18,7 @@ def fetch():
         return render_template('index.html', result=None, error=result['error'])
 
     return render_template('index.html', result=result, error=None)
-    
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
+
